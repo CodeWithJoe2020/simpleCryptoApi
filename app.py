ea49758eec070e2f8c4aa6748bddba2852d91cf5
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2023-02-13 20:46:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask,request,jsonify
 from web3 import Web3
 
-
-node= 'Your Node'
+node= 'Your node goes here'
 web3 = Web3(Web3.HTTPProvider(node)) 
 
 app = Flask(__name__)
@@ -19,13 +18,23 @@ def blocknumber():
     return jsonify({"blocknumber": web3.eth.block_number})
 
 
-
 @app.route("/nativeBalance/<address>")
 def getBalance(address):
     return jsonify({"nativeBalance":web3.eth.get_balance(address) / 10**18})
 
 
 
+@app.route("/getTransaction/<txHash>")
+def getTransaction(txHash):
+    transaction = web3.eth.get_transaction(txHash)
+    transaction_dict = dict(transaction)
+
+    # Convert bytes instances to hexadecimal string representations
+    for key, value in transaction_dict.items():
+        if isinstance(value, bytes):
+            transaction_dict[key] = value.hex()
+
+    return jsonify(transaction_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
